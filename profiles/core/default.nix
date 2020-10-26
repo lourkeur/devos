2d8d6a6ce1df1a5e8a51cb6d8700fa05d32e47ee
Louis <louis@bettens.info>
2020-10-26 18:10:02 +0100

    Remove Powerline fonts

--- profiles/core/default.nix
+++ profiles/core/default.nix
@@ -83,18 +83,6 @@ in
 
   };
 
-  fonts = {
-    fonts = with pkgs; [ powerline-fonts dejavu_fonts ];
-
-    fontconfig.defaultFonts = {
-
-      monospace = [ "DejaVu Sans Mono for Powerline" ];
-
-      sansSerif = [ "DejaVu Sans" ];
-
-    };
-  };
-
   nix = {
 
     autoOptimiseStore = true;
