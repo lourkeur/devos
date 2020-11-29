6cfaa076ccdc5032217472bc01cc8af2da544b30
Louis Bettens <louis@bettens.info>
2020-11-29 11:06:15 +0100

    Use louis in niximg

--- hosts/niximg.nix
+++ hosts/niximg.nix
@@ -1,15 +1,14 @@
 { modulesPath, ... }: {
   imports = [
-    # passwd is nixos by default
-    ../users/nixos
-    # passwd is empty by default
-    ../users/root
+    ../users/louis
     "${modulesPath}/installer/cd-dvd/iso-image.nix"
   ];
 
   isoImage.makeEfiBootable = true;
   isoImage.makeUsbBootable = true;
   networking.networkmanager.enable = true;
+
+  services.mingetty.autologinUser = "louis";
 
   isoImage.contents = [ {
     source = ../secrets/id_niximg;
